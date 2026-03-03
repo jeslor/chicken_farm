187f5856cbec7bf81f1152541d6f5fb69a115801
Jeslor Ssozi <jbestius@gmail.com>
2026-03-03 12:12:04 +0300

    change the lib actions to server side and user the env variable that will not be backed into next js code at build, eg without NEXT PUBLIC

--- chicken_farm_frontend/lib/actions/chicken.actions.ts
+++ chicken_farm_frontend/lib/actions/chicken.actions.ts
@@ -1,15 +1,18 @@
+'use server';
 import { Chicken } from '@/types/chicken';
 
-const BACKEND_URL = process.env.NEXT_PUBLIC_CHICKEN_API_URL || 'http://localhost:4000/api/chickens';
+//if running the frontend in Docker, use the internal Docker network address to reach the backend
+const BACKEND_URL =
+  process.env.CHICKEN_BACKEND_API_URL || 'http://host.docker.internal:4000/api/chickens';
 
 export async function getChickens(): Promise<Chicken[]> {
-  const res = await fetch(BACKEND_URL, { cache: 'no-store' });
+  const res = await fetch(BACKEND_URL!, { cache: 'no-store' });
   if (!res.ok) throw new Error('Failed to fetch chickens');
   return res.json();
 }
 
 export async function addChicken(data: Omit<Chicken, 'id' | 'createdAt'>): Promise<Chicken> {
-  const res = await fetch(BACKEND_URL, {
+  const res = await fetch(BACKEND_URL!, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
@@ -19,6 +22,6 @@ export async function addChicken(data: Omit<Chicken, 'id' | 'createdAt'>): Promi
 }
 
 export async function deleteChicken(id: string): Promise<void> {
-  const res = await fetch(`${BACKEND_URL}/${id}`, { method: 'DELETE' });
+  const res = await fetch(`${BACKEND_URL!}/${id}`, { method: 'DELETE' });
   if (!res.ok) throw new Error('Failed to delete chicken');
 }
